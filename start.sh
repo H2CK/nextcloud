30d46c2af5ff9f7168d600b8e37a0122fd7e1068
H2CK <t@jagel.net>
2021-01-08 22:09:22 +0100

    Optimize startup

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
- 
+/bin/01_user_config.sh
+/bin/02_auto_update.sh
+/bin/03_set_a2port.sh
+/bin/04_run_occ_commands.sh
+/bin/05_run_app_update.sh
+
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
