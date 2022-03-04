6f542d70d8e980c7163b83bd8e8fc593617fd12c
H2CK <H2CK@users.noreply.github.com>
2022-03-04 09:20:23 +0100

    Removed other architectures

--- docker.sh
+++ docker.sh
@@ -96,14 +96,14 @@ function docker_manifest_list_version() {
   echo "DOCKER MANIFEST: Create and Push docker manifest list - ${TARGET}:${BUILD_VERSION}."
 
   docker manifest create ${TARGET}:${BUILD_VERSION} \
-    ${TARGET}:${BUILD_VERSION}-amd64 \
-    ${TARGET}:${BUILD_VERSION}-arm32v7 \
-    ${TARGET}:${BUILD_VERSION}-arm64v8 #\
+    ${TARGET}:${BUILD_VERSION}-amd64 #\
+    # ${TARGET}:${BUILD_VERSION}-arm32v7 \
+    # ${TARGET}:${BUILD_VERSION}-arm64v8 #\
     # ${TARGET}:${BUILD_VERSION}-ppc64le \
     # ${TARGET}:${BUILD_VERSION}-s390x
 
-  docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-arm32v7 --os=linux --arch=arm --variant=v7
-  docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-arm64v8 --os=linux --arch=arm64 --variant=v8
+  # docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-arm32v7 --os=linux --arch=arm --variant=v7
+  # docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-arm64v8 --os=linux --arch=arm64 --variant=v8
   # docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-ppc64le --os=linux --arch=ppc64le
   # docker manifest annotate ${TARGET}:${BUILD_VERSION} ${TARGET}:${BUILD_VERSION}-s390x --os=linux --arch=s390x
   
@@ -123,14 +123,14 @@ function docker_manifest_list_test_beta_latest() {
   echo "DOCKER MANIFEST: Create and Push docker manifest list - ${TARGET}:${TAG_PREFIX}."
 
   docker manifest create ${TARGET}:${TAG_PREFIX} \
-    ${TARGET}:${BUILD_VERSION}-amd64 \
-    ${TARGET}:${BUILD_VERSION}-arm32v7 \
-    ${TARGET}:${BUILD_VERSION}-arm64v8 #\
+    ${TARGET}:${BUILD_VERSION}-amd64 #\
+    # ${TARGET}:${BUILD_VERSION}-arm32v7 \
+    # ${TARGET}:${BUILD_VERSION}-arm64v8 #\
     # ${TARGET}:${BUILD_VERSION}-ppc64le \
     # ${TARGET}:${BUILD_VERSION}-s390x
 
-  docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-arm32v7 --os=linux --arch=arm --variant=v7
-  docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-arm64v8 --os=linux --arch=arm64 --variant=v8
+  # docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-arm32v7 --os=linux --arch=arm --variant=v7
+  # docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-arm64v8 --os=linux --arch=arm64 --variant=v8
   # docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-ppc64le --os=linux --arch=ppc64le
   # docker manifest annotate ${TARGET}:${TAG_PREFIX} ${TARGET}:${BUILD_VERSION}-s390x --os=linux --arch=s390x
 
@@ -182,10 +182,10 @@ function prepare_qemu() {
   mkdir tmp
   pushd tmp &&
     curl -L -o qemu-x86_64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-x86_64-static.tar.gz && tar xzf qemu-x86_64-static.tar.gz &&
-    curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-arm-static.tar.gz && tar xzf qemu-arm-static.tar.gz &&
+    # curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-arm-static.tar.gz && tar xzf qemu-arm-static.tar.gz &&
     # curl -L -o qemu-ppc64le-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-ppc64le-static.tar.gz && tar xzf qemu-ppc64le-static.tar.gz &&
     # curl -L -o qemu-s390x-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-s390x-static.tar.gz && tar xzf qemu-s390x-static.tar.gz &&
-    curl -L -o qemu-aarch64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static.tar.gz && tar xzf qemu-aarch64-static.tar.gz &&
+    # curl -L -o qemu-aarch64-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/$QEMU_VERSION/qemu-aarch64-static.tar.gz && tar xzf qemu-aarch64-static.tar.gz &&
     popd
 }
 
