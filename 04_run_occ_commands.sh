a28f97995c3cfeee6b11e523a828767a4d0765e6
H2CK <dev@jagel.net>
2022-03-30 22:17:31 +0200

    Added missing settings for APCu on CLI
    Signed-off-by: H2CK <dev@jagel.net>

--- 04_run_occ_commands.sh
+++ 04_run_occ_commands.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo -u www-data php /var/www/html/occ upgrade -vvv >> /proc/1/fd/1 
-sudo -u www-data php /var/www/html/occ maintenance:update:htaccess -vvv >> /proc/1/fd/1 
+sudo -u www-data php --define apc.enable_cli=1 /var/www/html/occ upgrade -vvv >> /proc/1/fd/1 
+sudo -u www-data php --define apc.enable_cli=1 /var/www/html/occ maintenance:update:htaccess -vvv >> /proc/1/fd/1 
--- 05_run_app_update.sh
+++ 05_run_app_update.sh
@@ -2,5 +2,5 @@
 UPDT_APPS=${AUTO_UPDATE_APPS:-FALSE}
 if [ "$UPDT_APPS" = "TRUE" ]
 then
-sudo -u www-data php /var/www/html/occ app:update --all -vvv > /dev/stdout
+sudo -u www-data php --define apc.enable_cli=1 /var/www/html/occ app:update --all -vvv > /dev/stdout
 fi
